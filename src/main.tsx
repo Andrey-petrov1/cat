3b95546abc0614f5a5cc4a9efb10f291c3003db8
Андрей Петров <andrey_petrov_2017@mail.ru>
2025-05-11 14:29:51 +0300

    #3
    QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import RoutesComponent from "../src/routes/routes";
-import Header from "./page/header";
+ import ReactDOM from "react-dom/client";
+ 
+import App from "./App";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-   <> // тут должен быть контекнт
-      <BrowserRouter>
-         <Header />
-         <RoutesComponent />
-      </BrowserRouter>
-   </>
+  <QueryClientProvider client={queryClient}>
+  <App/>
+  </QueryClientProvider>
 );
